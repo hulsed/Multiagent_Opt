fa18b1957fbeadb77e3e6453b8ed2712123058dc
hulsed <hulsed@oregonstate.edu>
2017-01-19 10:28:24 -0800

    Added Test Matlab GA code

---
+++ GA_Experiment.m
@@ -0,0 +1,8 @@
+% GA Code
+ObjectiveFunction = @simple_fitness;
+nvars = 2;    % Number of variables
+LB = [0 0];   % Lower bound
+UB = [1 13];  % Upper bound
+ConstraintFunction = @simple_constraint;
+[x,fval] = ga(ObjectiveFunction,nvars,[],[],[],[],LB,UB, ...
+    ConstraintFunction)
