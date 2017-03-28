e71e4c45dc9f5f05b4deb2cf31d5eddda25976a9
hulsed <hulsed@oregonstate.edu>
2017-03-27 17:04:31 -0700

    rough prototype of continous action selection and learning completed

---
+++ choose_continousparamvals.m
@@ -0,0 +1,66 @@
+% Make agents choose an action
+
+% INPUTS
+% expMerit - the cell array of the expected merit of each choice for a variable, each cell containing a Q-table
+% completion - the completion of the learning cycle
+% TMin, the minimum temperature for 
+
+% OUTPUTS
+% x - column vector of integers, element i corresponds to the chosen value
+% for the variable i.
+function x = choose_continuousparamvals(expMerit, temps)
+    
+    % initialize vector of integers corresponding to the choices for each
+    % parameter
+    numChoices = numel(expMerit);
+    x = uint8(zeros(numChoices, 1));
+    
+    % Iterate through the variables
+    for ag = 1:numel(expMerit)
+        
+            % merit function for that specific variable
+            % NOTE: the sign is inverted so that the best value is chosen
+            % (since the best has the lowest objective value)
+            merit = -expMerit{ag};
+
+            T=temps(ag);
+            
+            %softmax normalization
+            merit2=1./(1+exp(-(merit-mean(merit))/(std(merit)+0.1)));
+            
+            %softmax selection (Note: a lower value in the merit function 
+            %corresponds to a lower value of the objective function, makeing
+            %)
+            p= exp(merit2./(T));                  
+            p=p/sum(p);
+            
+            %chooses value for the variable if softmax breaks
+            ChosenValue = find(isnan(p));
+            
+            if isempty(ChosenValue)
+                % Pick an action according to the probabilities in p
+                try
+                    ChosenValue = randsample(1:numel(merit), 1, true, p);
+                catch
+                    p % in case it produces a complex number
+                    ChosenValue = randsample(1:numel(merit), 1, true, p);
+                end
+            else
+                disp('Softmax broke due to infinite exponential!')
+                disp('Picking between three best')
+                [sorting,ranking]=sort(merit);
+                dice=randi(20,1);
+                if dice<=16
+                    ChosenValue=ranking(1);
+                elseif 16<dice<=19
+                    ChosenValue=ranking(2);
+                else 
+                    ChosenValue=ranking(3);
+                end
+                    
+            end
+
+            x(ag) = ChosenValue;   
+            clear p
+    end
+end
--- update_continousmerit.m
+++ update_continousmerit.m
@@ -1,43 +1,43 @@
-%function [meritfxn,oldptsx,oldptsy,learned]=update_continousmerit(oldptsx,oldptsy,xfound, yfound, UB,LB,tol,maxzones, Qinit)
+function [meritfxn,oldptsx,oldptsy,learned]=update_continousmerit(oldptsx,oldptsy,xfound, yfound, UB,LB,tol,maxzones, Qinit)
 
-xfound=[0.4,9,75];
-yfound=[0.99,0.99,0.99];
-
-xcont=rand(1,3);
-foundmerit=rand(1,3);
-
-%upper bounds
-UB(1)=1;
-UB(2)=10;
-UB(3)=100;
-%lower bounds
-LB(1)=0;
-LB(2)=1;
-LB(3)=50;
-%points stored already for each variable
-oldptsx{1}=rand(1,100)*(UB(1)-LB(1))+LB(1);
-oldptsx{2}=rand(1,100)*(UB(2)-LB(2))+LB(2);
-oldptsx{3}=rand(1,100)*(UB(3)-LB(3))+LB(3);
-
-oldptsy{1}=rand(1,100);
-oldptsy{2}=rand(1,100);
-oldptsy{3}=rand(1,100);
-
-%tolerance for each variable
-tol(1)=0.0001;
-tol(2)=0.001;
-tol(3)=0.001;
-%maximum number of zones for each variable
-maxzones(1)=20;
-maxzones(2)=20;
-maxzones(3)=20;
-%initial value
-Qinit=0;
+% xfound=[0.4,9,75];
+% yfound=[0.99,0.99,0.99];
+% 
+% xcont=rand(1,3);
+% foundmerit=rand(1,3);
+% 
+% %upper bounds
+% UB(1)=1;
+% UB(2)=10;
+% UB(3)=100;
+% %lower bounds
+% LB(1)=0;
+% LB(2)=1;
+% LB(3)=50;
+% %points stored already for each variable
+% oldptsx{1}=rand(1,100)*(UB(1)-LB(1))+LB(1);
+% oldptsx{2}=rand(1,100)*(UB(2)-LB(2))+LB(2);
+% oldptsx{3}=rand(1,100)*(UB(3)-LB(3))+LB(3);
+% 
+% oldptsy{1}=rand(1,100);
+% oldptsy{2}=rand(1,100);
+% oldptsy{3}=rand(1,100);
+% 
+% %tolerance for each variable
+% tol(1)=0.0001;
+% tol(2)=0.001;
+% tol(3)=0.001;
+% %maximum number of zones for each variable
+% maxzones(1)=20;
+% maxzones(2)=20;
+% maxzones(3)=20;
+% %initial value
+% Qinit=0;
 
 
 
 for ag=1:3
-   
+   learned(ag)=0;
    
    ptsx=oldptsx{ag};
    ptsy=oldptsy{ag};
@@ -54,7 +54,7 @@ for ag=1:3
         zptsx=xpts(xpts~=0);
         zptsy=ypts(xpts~=0);
         
-       [zonerepy(z),loc]=max(zptsy);
+       [zonerepy(z),loc]=min(zptsy);
        zonerepx(z)=zptsx(loc);
            
        else
@@ -64,8 +64,8 @@ for ag=1:3
        end
        % learned if better than other points in the zone
        if (zone{ag}(z)<xfound(ag) & xfound(ag)<=zone{ag}(z+1))
-           if yfound(ag)>zonerepy(z)
-               expImprovement(ag)=yfound(ag)-zonerepy(z);
+           if yfound(ag)<zonerepy(z)
+               expImprovement(ag)=zonerepy(z)-yfound(ag);
                learned(ag)=1;
                
                zonerepx(z)=xfound(ag);
@@ -101,4 +101,4 @@ end
 
 
 
-%end
+end
