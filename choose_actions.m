1371ce8a7dc3d6a46a68d10dd32daf36ed1cb212
hulsed <hulsed@oregonstate.edu>
2017-05-21 20:52:07 -0700

    deleted unnecessary comments

--- choose_actions.m
+++ choose_actions.m
@@ -19,37 +19,6 @@ function actions=choose_actions(values,T, epsilon)
                 ChosenAction=randi(numel(value));
             end
             
-            %softmax selection (Note: a lower value in the merit function 
-            %corresponds to a lower value of the objective function, makeing
-            %)
-%             p= exp(value);                  
-%             p=p/sum(p);
-%             
-%             %chooses value for the variable if softmax breaks
-%             ChosenAction = find(isnan(p));
-%             
-%             if isempty(ChosenAction)
-%                 % Pick an action according to the probabilities in p
-%                 try
-%                     ChosenAction = randsample(1:numel(value), 1, true, p);
-%                 catch
-%                     p % in case it produces a complex number
-%                     ChosenAction = randsample(1:numel(value), 1, true, p);
-%                 end
-%             else
-%                 disp('Softmax broke due to infinite exponential!')
-%                 disp('Picking between three best')
-%                 [sorting,ranking]=sort(value);
-%                 dice=randi(20,1);
-%                 if dice<=16
-%                     ChosenAction=ranking(1);
-%                 elseif 16<dice<=19
-%                     ChosenAction=ranking(2);
-%                 else 
-%                     ChosenAction=ranking(3);
-%                 end
-%                     
-             %end
             
             actions(ag) = ChosenAction;   
             clear p
