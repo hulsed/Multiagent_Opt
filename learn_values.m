9b065dcac7820b1a227f817989621b7a1cd2facb
hulsed <hulsed@oregonstate.edu>
2017-08-22 11:40:32 -0700

    initial commmit for rewarding based on objective improvement only

--- learn_values.m
+++ learn_values.m
@@ -1,11 +1,11 @@
-function values=learn_values(values,actions,rewards,alpha);
+function values=learn_values(values,actions,reward,alpha);
     
     % Iterate through agents value tables
     for ag = 1:numel(values)
         % Get the current merit of the parameter value chosen in previous state
         Q = values{ag}(actions(ag));
              
-        values{ag}( actions(ag)) = Q + alpha*(rewards(ag) - Q);
+        values{ag}( actions(ag)) = Q + alpha*(reward - Q);
     end
 
 end
